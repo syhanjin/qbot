ecee0dcfc5f7834ba60be6d39191d3ec8e2d577e
syhanjin <2819469337@qq.com>
2021-09-01 19:15:21 +0800

    bug

--- qbot_main.py
+++ qbot_main.py
@@ -83,7 +83,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    logging_put('request='+request)
+    logging_put('request='+str(request))
     return 200
     while True:
         msg = sever.rev_msg()
