d870a1f24195c18fb64ca0d14d01a8f8c3feadc3
syhanjin <2819469337@qq.com>
2021-09-01 22:01:51 +0800

    group_ban_all

--- handles/operations.py
+++ handles/operations.py
@@ -13,10 +13,18 @@ def logging_put(info):
     )
     logging.info(info)
 
-def group_ban(group_id, user_id, duration):
-    payload = '/set_group_ban?group_id='+str(group_id)+'&user_id='+str(user_id)+'&duration='+str(duration)
-    logging_put('群['+str(group_id)+']禁言['+str(user_id)+']'+str(duration)+'秒')
-    r = requests.get('http://127.0.0.1:5700' + payload)
+def group_ban(group_id, user_id, duration=0):
+    if user_id == 'all':
+        if duration != 0:
+            r = requests.get('/set_group_whole_ban?group_id='+str(group_id)+'&enable=true')
+            logging_put('群['+str(group_id)+']禁言')
+        else:
+            r = requests.get('/set_group_whole_ban?group_id='+str(group_id)+'&enable=false')
+            logging_put('群['+str(group_id)+']解禁')
+    else:
+        payload = '/set_group_ban?group_id='+str(group_id)+'&user_id='+str(user_id)+'&duration='+str(duration)
+        logging_put('群['+str(group_id)+']禁言['+str(user_id)+']'+str(duration)+'秒')
+        r = requests.get('http://127.0.0.1:5700' + payload)
     if r.status_code != 200:
         logging_put('禁言失败')
         return False
