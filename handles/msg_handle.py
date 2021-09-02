7f75bed79cc555e2241e045a7b1287d81674d167
syhanjin <2819469337@qq.com>
2021-09-02 14:45:27 +0800

    bug

--- handles/msg_handle.py
+++ handles/msg_handle.py
@@ -3,48 +3,45 @@
 
 
 # 获取上报类型：message、notice、request
-def get_post_type(msg): return msg['post_type']
+def get_post_type(msg): return msg.get('post_type')
 
 
 # 获取信息类型 群聊/私聊 group/private
-def get_message_type(msg): return msg['message_type']
+def get_message_type(msg): return msg.get('message_type')
 
 
 # 获取群号/私聊qq号
 def get_number(msg):
     if get_message_type(msg) == 'group':
-        return msg['group_id']
+        return msg.get('group_id')
     elif get_message_type(msg) == 'private':
-        return msg['user_id']
+        return msg.get('user_id')
 
 
 # 获取角色
-def get_role(msg): return msg['sender']['role']
+def get_role(msg): return msg.get('sender').get(
+    'role') if (msg.get('sender')) else None
 
 
 # 获取群号
-def get_group_id(msg): return msg['group_id']
+def get_group_id(msg): return msg.get('group_id')
 
 
 # 获取QQ号
-def get_user_id(msg): return msg['user_id']
+def get_user_id(msg): return msg.get('user_id')
 
 
 # 获取信息发送者的QQ号
-def get_user_id(msg): return msg['user_id']
+def get_user_id(msg): return msg.get('user_id')
 
 
 # 获取发送的信息
-def get_raw_message(msg): return msg['raw_message']
-
-
-# 改变发送的信息
-def set_raw_message(msg, new_info): msg['raw_message'] = new_info
+def get_raw_message(msg): return msg.get('raw_message')
 
 
 # 得到通知类型
-def get_notice_type(msg): return msg['notice_type']
+def get_notice_type(msg): return msg.get('notice_type')
 
 
 # 得到请求类型
-def get_request_type(msg): return msg['request_type']
+def get_request_type(msg): return msg.get('request_type')
