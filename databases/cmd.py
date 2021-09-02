245ca6883bfcab9a4e0c0f84dcd36189dbd265a5
syhanjin <2819469337@qq.com>
2021-09-02 12:56:09 +0800

    bug

--- databases/cmd.py
+++ databases/cmd.py
@@ -8,7 +8,7 @@ db.drop_collection('cmd')
 db.create_collection('cmd')
 db.cmd.insert_many([
     {'key': '^签到$', 'value': 'sign', 'type': 'function', 'group': True},
-    {'key': '^群名片统计$', 'value': 'cards',
+    {'key': '^群名片统计$', 'value': 'test_cards',
         'type': 'function', 'inline': True, 'group': True},
     {'key': '^禁言 *\[CQ:at,qq=(\d+|all)[^\]]*\] *(\d+)?$', 'value': 'group_ban',
      'type': 'function', 'group': True, 'inline': True},
